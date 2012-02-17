8339ffadfb414f4e4978e5f39f14de2010e78a75
Erin Lawrence <el2891@gmail.com>
2012-02-17 14:47:21 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TkwaApp::Application.config.secret_token = '383a771497f790e95d1faf5b596da8f1039989808ebf3a8edf72df0fc5119cbd6452c58efabc486d4e92651499e1a65b628c7e35217c4bd95d42875b4c7405d5'
