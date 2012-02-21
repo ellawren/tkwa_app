a5d522b835d70810d813c90198fa2dfb3ee536d9
Erin Lawrence <el2891@gmail.com>
2012-02-21 09:36:40 -0600

    finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,7 +19,7 @@ describe "User pages" do
     it { should have_selector('title', text: full_title('Sign up')) }
   end
   
-describe "signup" do
+  describe "signup" do
 
     before { visit signup_path }
 
@@ -40,6 +40,18 @@ describe "signup" do
       it "should create a user" do
         expect { click_button "Sign up" }.to change(User, :count).by(1)
       end
+      
+      describe "after saving the user" do
+        before { click_button "Sign up" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.flash.success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+      
     end
+    
   end
+  
 end
