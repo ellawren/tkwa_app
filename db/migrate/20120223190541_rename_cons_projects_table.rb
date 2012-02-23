ea14ecea46719e61e2b64891f1c027cb9546df50
Erin Lawrence <el2891@gmail.com>
2012-02-23 14:17:45 -0600

    removed db migration

--- db/migrate/20120223190541_rename_cons_projects_table.rb
+++
@@ -1,9 +0,0 @@
-class RenameConsProjectsTable < ActiveRecord::Migration
-  def up
-  	rename_table :cons_projects, :consultant_roles_projects
-  end
-
-  def down
-  	rename_table :consultant_roles_projects, :cons_projects
-  end
-end
