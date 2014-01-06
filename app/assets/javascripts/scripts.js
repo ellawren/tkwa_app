5f85752fed05b92fa002f3b302faee950b3d5e8b
Erin Lawrence <el2891@gmail.com>
2014-01-06 16:47:18 -0600

    heroku

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,88 +1,86 @@
 $(document).ready(function() {
 
-  $("a.tip").tooltip({ animation: true, placement: 'top', trigger:'hover' });
-  $("a.tip-right").tooltip({ animation: true, placement: 'right', trigger:'hover' });
-  $("a.tip-bottom").tooltip({ animation: true, placement: 'bottom', trigger:'hover' });
+    $("a.tip").tooltip({ animation: true, placement: 'top', trigger:'hover' });
+    $("a.tip-right").tooltip({ animation: true, placement: 'right', trigger:'hover' });
+    $("a.tip-bottom").tooltip({ animation: true, placement: 'bottom', trigger:'hover' });
+    $(".phases .checkbox .icons").html("10");
 
- // main nav hover controls
-    $("ul#nav li.active").hover(function () {
-      $(this).children(".control-icons").stop().fadeToggle("slow");
-    });
-    
-  // hover function on search results - projects/contacts index
-  $('tr.results-row').click( function() {
-      window.location = $(this).find('a').attr('href');
-  }).hover( function() {
-      $(this).toggleClass('hover');
-  });
+     // main nav hover controls
+        $("ul#nav li.active").hover(function () {
+          $(this).children(".control-icons").stop().fadeToggle("slow");
+        });
+        
+      // hover function on search results - projects/contacts index
+      $('tr.results-row').click( function() {
+          window.location = $(this).find('a').attr('href');
+      }).hover( function() {
+          $(this).toggleClass('hover');
+      });
 
-  // edit button on pattern projects page
-  $('.edit-pattern').click( function() {
-    $(this).parent('.view-fields').hide();
-    $(this).parent().parent('.pattern').children('.edit-fields').show();
-  });
+      // edit button on pattern projects page
+      $('.edit-pattern').click( function() {
+        $(this).parent('.view-fields').hide();
+        $(this).parent().parent('.pattern').children('.edit-fields').show();
+      });
 
-  $('.text-button').click( function() {
-    $(this).parent('.edit-fields').hide();
-    $(this).parent().parent('.pattern').children('.view-fields').show();
-  });
+      $('.text-button').click( function() {
+        $(this).parent('.edit-fields').hide();
+        $(this).parent().parent('.pattern').children('.view-fields').show();
+      });
 
-  // show personal info on contacts page
-  $('#show').click( function() {
-    $('#personal-info.hidden').slideDown("slow");
-    $('#show').hide();
-    $('#hide').show();
-  });
+      // show personal info on contacts page
+      $('#show').click( function() {
+        $('#personal-info.hidden').slideDown("slow");
+        $('#show').hide();
+        $('#hide').show();
+      });
 
-  $('#hide').click( function() {
-    $('#personal-info.hidden').slideUp("slow");
-    $('#show').show();
-    $('#hide').hide();
-  });
+      $('#hide').click( function() {
+        $('#personal-info.hidden').slideUp("slow");
+        $('#show').show();
+        $('#hide').hide();
+      });
 
-  // edit row on timesheets
-  $('.edit-me').click( function() {
-    id = $(this).parent().parent(".table-row").attr("id");
-    $("#" + id).addClass("edit");
-  });
+      // edit row on timesheets
+      $('.edit-me').click( function() {
+        id = $(this).parent().parent(".table-row").attr("id");
+        $("#" + id).addClass("edit");
+      });
 
-   $('.done-editing').click( function() {
-    id = $(this).parent().parent(".table-row").attr("id");
-    $("#" + id).removeClass("edit");
-  });
+       $('.done-editing').click( function() {
+        id = $(this).parent().parent(".table-row").attr("id");
+        $("#" + id).removeClass("edit");
+      });
 
-  $( ".datepicker" ).datepicker();
+      $( ".datepicker" ).datepicker();
 
-  $('#add-message').click( function() {
-    $('#new-message').show();
-    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-    $('#message-content').focus();
-  });
+      $('#add-message').click( function() {
+        $('#new-message').show();
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+        $('#message-content').focus();
+      });
 
-  $('#cancel-message').click( function() {
-    $('#new-message').slideUp();
-    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-  });
+      $('#cancel-message').click( function() {
+        $('#new-message').slideUp();
+        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+      });
 
-  $('a.more-options').click( function() {
-    $('#more-options').slideDown();
-    $(this).hide();
-    $('a.less-options').show();
-  });
+      $('a.more-options').click( function() {
+        $('#more-options').slideDown();
+        $(this).hide();
+        $('a.less-options').show();
+      });
 
-  $('a.less-options').click( function() {
-    $('#more-options').slideUp();
-    $(this).hide();
-    $('a.more-options').show();
-    $('#q_work_address_or_home_address_cont').val("");
-    $('#q_work_email_or_home_email_cont').val("");
-    $('#q_home_phone_or_work_cell_or_home_cell_or_work_direct_or_work_phone_cont').val("");    
-  });
+      $('a.less-options').click( function() {
+        $('#more-options').slideUp();
+        $(this).hide();
+        $('a.more-options').show();
+        $('#q_work_address_or_home_address_cont').val("");
+        $('#q_work_email_or_home_email_cont').val("");
+        $('#q_home_phone_or_work_cell_or_home_cell_or_work_direct_or_work_phone_cont').val("");    
+      });
 
 });
-
-  //scope
-$(".phases .checkbox .icons").html("10");
 
 
 $(document).ready(function() {
