8aa1a3af70aff2443c533934a2be738cfff2ed16
Erin Lawrence <el2891@gmail.com>
2014-01-17 13:28:12 -0600

    paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,11 +70,11 @@ TkwaApp::Application.configure do
 
   # AWS settings for Paperclip assets
   config.paperclip_defaults = {
-  :storage => :s3,
+    :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['Kubala_Washatko_Architects'],
-      :access_key_id => ENV['AKIAIQOJJZYTNAZ4GCCQ'],
-      :secret_access_key => ENV['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']
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
   
