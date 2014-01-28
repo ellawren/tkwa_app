a0cd9796291bc59d085b569fee5dccf8af4bd7af
Erin Lawrence <el2891@gmail.com>
2014-01-28 13:39:33 -0600

    add to project list button

--- app/controllers/employee_teams_controller.rb
+++ app/controllers/employee_teams_controller.rb
@@ -1,27 +1,38 @@
 class EmployeeTeamsController < ApplicationController
   
-
-  def edit
-    @employee_team = EmployeeTeam.find(params[:id])
-    #@contact = Contact.find(@employee_team.contact_id)
-    @user = User.find(@employee_team.user_id)
-    @project = Project.find(@employee_team.project_id)
-    @phases = @project.available_phases
-    render :layout => 'modal' 
-  end
-
-  def update
-    @employee_team = EmployeeTeam.find(params[:id])
-    if @employee_team.update_attributes(params[:employee_team])
-      #respond_to do |format|
-      #  format.js { render :js => "my_function();" }
-      #  redirect_to tracking_project_path(Project.find(@employee_team.project_id))
-      #end
-      flash[:success] = "Team updated successfully!"
-      redirect_to tracking_project_path(Project.find(@employee_team.project_id))
-    else
-      redirect_to(:back) 
+    def new
+        @employee_team = EmployeeTeam.new
+        @user = User.find(params[:user_id])
+        @employee_team.user_id = params[:user_id]
+        @projects = Project.where(:status => "current").order('number ASC')
+        render :layout => 'modal' 
     end
-  end
+  
+    def edit
+        @employee_team = EmployeeTeam.find(params[:id])
+        @user = User.find(@employee_team.user_id)
+        @project = Project.find(@employee_team.project_id)
+        @phases = @project.available_phases
+        render :layout => 'modal' 
+    end
+
+    def update
+        @employee_team = EmployeeTeam.find(params[:id])
+        if @employee_team.update_attributes(params[:employee_team])
+              flash[:success] = "Team updated successfully!"
+              redirect_to tracking_project_path(Project.find(@employee_team.project_id))
+        else
+            redirect_to(:back) 
+        end
+    end
+
+    def create
+        @employee_team = EmployeeTeam.new(params[:employee_team])
+        if @employee_team.save
+            redirect_to(:back) 
+        else
+            render 'new'
+        end
+    end
   
 end
