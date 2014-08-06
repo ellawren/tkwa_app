bf28b70b3f1cc5dadb56e51afdebf57cb1e6658b
ellawren <elawrence@outlook.com>
2014-08-06 12:19:06 -0500

    wkhtmltopdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,7 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  :exe_path => '/usr/local/bin/wkhtmltopdf'
-}
+WickedPdf.config do |config|  
+  	if Rails.env == 'production' then
+    	config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf-amd64"
+  	else 
+    	config.exe_path = '/usr/local/bin/wkhtmltopdf'
+  	end
+end
